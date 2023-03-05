1d413830c0d426a49e01419c1133cc077fbda7f0
Jeff Hyatt <jhyatt00@gmail.com>
2023-03-05 14:38:56 -0500

    add modify date

--- script1_ca.py
+++ script1_ca.py
@@ -1,4 +1,5 @@
-#from cloudacademy python course, updated for python 3
+# from cloudacademy python course, updated for python 3
+# modified 3/5/2023
 
 my_name = "Patrick Bateman"
 print("Hello and welcome " + my_name + "!")
