2f704c99f2fb0b60ee34bce14cbd1b9de4b115c6
Jeff Hyatt <jhyatt00@gmail.com>
2023-03-05 14:44:26 -0500

    print meal exercise

---
+++ print-meal.py
@@ -0,0 +1,20 @@
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal = "A protein bar"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = "A sandwich and chips"
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+meal = "Pasta!"
+
+# Printing out dinner
+print("Dinner:")
+print(meal)
