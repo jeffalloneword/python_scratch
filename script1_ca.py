652c7152b70f7b3200c3701debc5ed0a6414fbae
Jeff Hyatt <jhyatt00@gmail.com>
2023-03-06 16:25:10 -0500

    more number operations

--- script1_ca.py
+++ script1_ca.py
@@ -3,3 +3,18 @@
 
 my_name = "Patrick Bateman"
 print("Hello and welcome " + my_name + "!")
+
+quilt_width = 8
+quilt_length = 8
+
+print(quilt_width * quilt_length)
+
+# Calculation of squares for:
+# 6x6 quilt
+print(6 ** 2)
+# 7x7 quilt
+print(7 ** 2)
+# 8x8 quilt
+print(8 ** 2)
+# How many squares for 6 people to have 6 quilts each that are 6x6?
+print(6 ** 4)
