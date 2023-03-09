034e88b20c7def4fbe5450401cb93519f9565e49
Jeff Hyatt <jhyatt00@gmail.com>
2023-03-08 19:05:25 -0500

    plus-equals operator

--- script1_ca.py
+++ script1_ca.py
@@ -40,3 +40,19 @@ print(full_birthday_string)
  
 # This also prints "I am 10 years old today!"
 print(birthday_string, age, birthday_string_2)
+
+# ********************************************************************
+
+total_price = 0
+
+new_sneakers = 50.00
+
+total_price += new_sneakers
+
+nice_sweater = 39.00
+fun_books = 20.00
+# Update total_price here:
+total_price += nice_sweater
+total_price += fun_books
+
+print("The total price is", total_price)
